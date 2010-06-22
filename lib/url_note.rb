eb9a3dec9363780422f84ddea4c484be0852bc16
Dean Hudson <dean@ero.com>
2010-06-22 01:11:43 -0700

    Fixed a bug in /post.

--- lib/url_note.rb
+++ lib/url_note.rb
@@ -1,19 +1,23 @@
 require 'rubygems'
 require 'active_record'
 
-class UrlNote < ActiveRecord::Base; end
+class UrlNote < ActiveRecord::Base
+  def self.create_db
+    ActiveRecord::Base.logger = Logger.new(STDERR)
+    ActiveRecord::Base.establish_connection(
+        :adapter => "sqlite3",
+        :database  => "../db/notes.db"
+    )
 
-if __FILE__ == $0
-  ActiveRecord::Base.logger = Logger.new(STDERR)
-  ActiveRecord::Base.establish_connection(
-      :adapter => "sqlite3",
-      :database  => "../db/notes.db"
-  )
-
-  ActiveRecord::Schema.define do
-    create_table :url_notes do |table|
-      table.column :url, :string
-      table.column :text, :text
+    ActiveRecord::Schema.define do
+      create_table :url_notes do |table|
+        table.column :url, :string
+        table.column :text, :text
+      end
     end
   end
+end
+
+if __FILE__ == $0
+  UrlNote.create_db
 end
